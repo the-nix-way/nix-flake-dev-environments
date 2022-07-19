e2732c6d075f1124c6a131dc7a9a8ba5bd616868
Luc Perkins <lucperkins@gmail.com>
2022-07-20 01:59:50 +0200

    Add default package derivation

--- ruby-on-rails/flake.nix
+++ ruby-on-rails/flake.nix
@@ -4,9 +4,10 @@
   inputs = {
     nixpkgs.url = "nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
+    nix-filter.url = "github:numtide/nix-filter";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, nix-filter }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
@@ -20,8 +21,40 @@
           copyGemFiles = true; # Not sure if this is necessary
         };
 
-        inherit (pkgs) bundlerEnv mkShell; # Inheritance helper
+        inherit (nix-filter.lib) filter;
+        inherit (pkgs) bundlerEnv mkShell;
+        inherit (pkgs.stdenv) mkDerivation;
       in {
+        apps = {
+          default = {
+            type = "app";
+            program = "${self.packages.${system}.default}/bin/rails";
+          };
+        };
+
+        packages = rec {
+          default = railsApp;
+
+          railsApp = mkDerivation {
+            name = "nix-flake-rails";
+            src = filter {
+              root = ./.;
+              exclude = [
+                ./flake.lock
+                ./flake.nix
+                ./gemset.nix
+                ./README.md
+              ];
+            };
+            buildInputs = [ rubyEnv rubyEnv.wrappedRuby ] ++ (with pkgs; [ bundler ]);
+            installPhase = ''
+              mkdir -p $out
+              mkdir -p $out/tmp/{cache,pids,sockets}
+              cp -r $src/* $out/
+            '';
+          };
+        };
+
         devShells = rec {
           default = run;
 
